a591d4de72473c41ee3b00b083b30e0e212a168e
Wahid Hidayat <wshidayat38@gmail.com>
2022-01-03 18:49:34 +0700

    Initial Commit

---
+++ Blog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Blog
+//
+//  Created by Wahid Hidayat on 03/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
